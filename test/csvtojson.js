e1c918001b47d45fd7de988eda4aa9e3d89f3c56
Jhamm <julianhamm1@gmail.com>
2019-07-06 02:57:59 +0100

    Initial commit and search req endpoint

--- test/csvtojson.js
+++
@@ -1,6 +0,0 @@
-let csvToJson = require('convert-csv-to-json');
-
-let json = csvToJson.fieldDelimiter(',').formatValueByType().getJsonFromCsv("./test.csv");
-for (let i = 0; i < json.length; i++) {
-    console.log(JSON.stringify(json[i]));
-}
